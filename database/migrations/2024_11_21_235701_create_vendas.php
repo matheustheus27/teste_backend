b5f9c41935a091b4554c6797aa8c2147bae52c2c
Matheus Thiago <31577877+matheustheus27@users.noreply.github.com>
2024-11-21 21:07:19 -0300

    Feat: migrations

---
+++ database/migrations/2024_11_21_235701_create_vendas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table->string('cliente');
+            $table->unsignedBigInteger('produto');
+            $table->integer('quantidade');
+            $table->decimal('valor_unitario', 10, 2);
+            $table->decimal('valor_total', 10, 2);
+            $table->date('dt_compra');
+            $table->timestamps();
+
+            $table->foreign('cliente')->references('cpf')->on('usuarios')->onDelete('cascade');
+            $table->foreign('produto')->references('cod')->on('produtos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendas');
+    }
+};
